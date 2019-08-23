08b036ff26a7f396d58168ea79ad34f5d482c8a9
Code0N <redeemer.of.souls@yandex.ru>
2019-08-23 18:00:22 +0300

    Готова базовая функциональность

--- app/src/main/java/code0n/androrselexter/MainActivity.kt
+++ app/src/main/java/code0n/androrselexter/MainActivity.kt
@@ -2,11 +2,59 @@ package code0n.androrselexter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.appcompat.app.AlertDialog
+import android.R.string.cancel
+import android.content.DialogInterface
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clearOE(view: View) {
+        optET.setText("")
+    }
+
+    fun randomSelect(view: View) {
+        fun getLines(view: TextView): List<CharSequence> {
+            var lines: MutableList<CharSequence> = mutableListOf()
+            val lay: Layout = view.layout
+            if (lay != null) {
+                var lineCount = lay.lineCount
+                var text: CharSequence = lay.text
+                var i = 0
+                var startIndex = 0
+                while (i < lineCount) {
+                    var endIndex = lay.getLineEnd(i)
+                    lines.add(text.subSequence(startIndex, endIndex))
+                    startIndex = endIndex
+                    i++
+                }
+            }
+            return lines
+        }
+        val lines = getLines(optET)
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("Случайный выбор")
+            .setMessage(lines[Random().nextInt(lines.size)].toString())
+            .setCancelable(false)
+            .setNegativeButton("OK",
+                DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+        val alert = builder.create()
+        alert.show()
     }
 }
