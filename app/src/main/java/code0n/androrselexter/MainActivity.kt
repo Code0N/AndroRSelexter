594e32f38f899e6c48dae3f19dd67820e0d3f2b8
Code0N <redeemer.of.souls@yandex.ru>
2019-08-23 16:34:18 +0300

    Initial commit

---
+++ app/src/main/java/code0n/androrselexter/MainActivity.kt
@@ -0,0 +1,12 @@
+package code0n.androrselexter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
